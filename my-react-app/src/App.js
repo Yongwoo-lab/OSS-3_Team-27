0fa9a98123fe149574dc8bb7d4ced0ddd918ec0b
Yongwoo-lab <joyful1108@naver.com>
2024-11-30 17:19:46 +0900

    main/login page added

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,10 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Main from './components/Main';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-
+<Routes>
+        <Route path="/" element={<Main/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ my-react-app/src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+      메인 페이지  지  지  
+    </div>
+  )
+}
+
+export default Main
