f86aa8f1c6676b47a9c41608b0185a3714a05304
Yujin Kim <yjkimliz03@gmail.com>
2024-12-01 16:32:02 +0900

    Refine conflict

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,16 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-<<<<<<< HEAD
 import Main from './pages/Main';
 import Login from './pages/Login';
 import List from './pages/List';
+import Register from './components/Register';
 import Header from './components/Header';
 import { useState } from 'react';
-=======
-import Main from './components/Main';
-import Login from './components/Login';
-import Register from './components/Register';
->>>>>>> ce8672e5c552ab7e5a9dccd82a542391759b9148
 
 function App() {
   // 지역 선택 및 검색 상태 관리
@@ -31,7 +26,6 @@ function App() {
 
   return (
     <div className="App">
-<<<<<<< HEAD
       <Header onRegionClick={handleRegionClick} onSearch={handleSearch} />
       <Routes>
         <Route
@@ -40,12 +34,8 @@ function App() {
         />
         <Route path="/login" element={<Login />} />
         <Route path="/list" element={<List />} />
-=======
-      <Routes>
-        <Route path="/" element={<Main/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
->>>>>>> ce8672e5c552ab7e5a9dccd82a542391759b9148
+        <Route path="/register" element={<Register />} />
+        <Route path="/header" element={<Header />} />
       </Routes>
     </div>
   );
